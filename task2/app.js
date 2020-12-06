f2a2518fc29633e3299cca2b64bebddfe6e22086
Ihor SS <62531809+Sirkevich@users.noreply.github.com>
2020-12-06 13:52:48 +0200

    add func getCopyObjKeys

---
+++ task2/app.js
@@ -0,0 +1,16 @@
+function getCopyObjKeys(obj) {
+  for (let prop in obj) {
+    obj[prop] = "";
+  }
+  return obj;
+}
+
+const transport = {
+  bmw: "auto",
+  audi: "auto",
+  bugatti: "motorcycle",
+  "bmw-price": 5000,
+  "audi-price": 8000,
+};
+ 
+console.log(getCopyObjProperties(transport));
